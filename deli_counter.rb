763a624a618bdc8d8f8f7b940b2133e4609a1e84
Philippe Timothe <philippetimothe@gmail.com>
2020-03-12 23:18:35 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,15 @@
-# Write your code here.
+katz_deli = []
+  
+def line(katz_deli)
+  if katz_deli.length == 0
+    puts "The line is currently empty."
+  end
+end
+
+def take_a_number(katz_deli, name)
+    counter = 1
+    katz_deli.each do |line|
+    puts "The line is currently: #{counter}. #{line}"
+    counter += 1
+  end
+end
