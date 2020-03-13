5e520b0f2575a54b4c171618a35184ca5f1a173c
Philippe Timothe <philippetimothe@gmail.com>
2020-03-13 02:23:26 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -23,7 +23,8 @@ end
 def now_serving(other_deli)
   if katz_deli.length == 0 
     puts "There is nobody waiting to be served!"
-  else
-  puts "Currently serving ."
-end
+  end
+  
+  if katz_deli.length > 0
+    puts "currently serving #{other_deli}"  
 end
