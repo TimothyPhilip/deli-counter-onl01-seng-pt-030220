ec1ad885910c1ca4ad1f1439296219d53a12a333
Philippe Timothe <philippetimothe@gmail.com>
2020-03-13 01:51:53 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,13 +6,12 @@ def line(katz_deli)
   if katz_deli.length == 0
     puts "The line is currently empty."
   end
-  katz_deli.each_with_index { |x, index| "#{index+1}. #{x}
+  katz_deli.each_with_index { |x, index| katz_deli_new << "#{index+1}. #{x}" }
   counter += 1
  
-  #if katz_deli.length > 0 
-   #  puts "The line is currently: " + katz_deli_new.join("  ")
-    
-  end
+  if katz_deli.length > 0 
+     puts "The line is currently: " + katz_deli_new.join("  ")
+    end
 end
   
   
