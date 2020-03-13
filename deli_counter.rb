87615b2f427a46fba2a6b0562ec906691504d683
Philippe Timothe <philippetimothe@gmail.com>
2020-03-13 01:48:10 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -8,10 +8,9 @@ def line(katz_deli)
   end
   katz_deli.each do |x| puts katz_deli_new << "#{counter}" + x 
   counter += 1
-end
  
-  if katz_deli.length > 0 
-     puts "The line is currently: " + katz_deli_new.join("  ")
+  #if katz_deli.length > 0 
+   #  puts "The line is currently: " + katz_deli_new.join("  ")
     
   end
 end
