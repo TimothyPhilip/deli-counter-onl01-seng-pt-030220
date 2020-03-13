4a0c5aea5b05e9fb49d8e6e2249a13a7a06c91a7
Philippe Timothe <philippetimothe@gmail.com>
2020-03-13 00:06:01 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,10 +1,12 @@
 katz_deli = []
   
 def line(katz_deli)
+  counter = 1
   if katz_deli.length == 0
     puts "The line is currently empty."
     elsif katz_deli.legth > 0
-    puts 
+    puts "The line is currently: #{counter}. #{katz_deli}"
+    counter += 1
   end
 end
 
