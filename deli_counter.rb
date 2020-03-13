4fead7ca1099a04324e822dfaeff6ba69f39f435
Philippe Timothe <philippetimothe@gmail.com>
2020-03-13 02:29:19 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -24,7 +24,7 @@ def now_serving(other_deli)
   if other_deli.length == 0 
     puts "There is nobody waiting to be served!"
   else
-    puts "Currently serving #{other_deli}"
+    puts "Currently serving #{other_deli.shift}"
   end
   
 
