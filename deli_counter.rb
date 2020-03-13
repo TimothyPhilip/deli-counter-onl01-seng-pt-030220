611481c966132942d25ef9a4ca9228557ee571be
Philippe Timothe <philippetimothe@gmail.com>
2020-03-13 00:41:56 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,14 +5,5 @@ def line(katz_deli)
   if katz_deli.length == 0
     puts "The line is currently empty."
   end
-  katz_deli_new = []
-  katz_deli.each { |x| katz_deli_new << "#{counter}." + x }
-    counter += 1
-    
-  end
   
-end
-
-def take_a_number(katz_deli, name)
-    
-end
+  
