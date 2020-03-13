482dabff0faa511c998d9df247fbed62ed3d238a
Philippe Timothe <philippetimothe@gmail.com>
2020-03-13 01:16:33 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,7 +6,7 @@ def line(katz_deli)
   if katz_deli.length == 0
     puts "The line is currently empty."
   elsif katz_deli.length > 0 
-    puts "The line is currently: #{counter}." + katz_deli.join()
+    puts "The line is currently: #{counter}." + katz_deli.join(" , ")
   end
 end
   
