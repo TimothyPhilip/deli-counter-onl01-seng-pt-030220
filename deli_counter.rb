0c204ed72894ae8cb7d09e396c8e20e8e9b67a7d
Philippe Timothe <philippetimothe@gmail.com>
2020-03-13 02:03:20 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -17,5 +17,5 @@ end
   
 def take_a_number(katz_deli,name)
   katz_deli.push(name)
-  puts "Welcome, #{name}. You are number 1 in line."
+  puts "Welcome, #{name}. You are number #{katz_deli.index()} in line."
 end
